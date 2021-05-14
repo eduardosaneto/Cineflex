21b6837e1fb0997b6b6f21090fac0b3b4f752e47
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-05-14 18:57:43 -0300

    Botão de voltar à página anterior escondido da home e da página final

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 import { BsBoxArrowLeft } from "react-icons/bs";
 
 export default function Header() {
 
+    const location = useLocation();
+    console.log(location);
     const history = useHistory();
 
     function goBack() {
         history.goBack();
     }
 
-    return (
-        <header>
-            <BsBoxArrowLeft className="return" onClick={goBack}/>
-            <Link to="/">
-                <h1>CINEFLEX</h1>
-            </Link>
-        </header>
-    );
+    if(location.pathname !== "/" && location.pathname !== "/success"){
+        return (
+            <header>
+                <BsBoxArrowLeft className="return" onClick={goBack}/>
+                <Link to="/">
+                    <h1>CINEFLEX</h1>
+                </Link>
+            </header>
+        );
+    } else {
+        return (
+            <header>
+                <Link to="/">
+                    <h1>CINEFLEX</h1>
+                </Link>
+            </header>
+        );
+    }
+
+
 }
